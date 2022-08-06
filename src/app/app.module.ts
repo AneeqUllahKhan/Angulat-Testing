9adaa86451ec211939ff9afe567dc7e16d5622d7
Aneeq Ullah Khan <aneequllah.khan@outlook.com>
2022-08-06 15:16:10 +0500

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './MyComponents/about/about.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
+import { FooterComponent } from './MyComponents/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
